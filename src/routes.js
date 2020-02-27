2a2c661a6422bb9cec8e5908c43a00c6bd315f03
Euller Cristian <eullercdr@gmail.com>
2020-02-27 14:20:29 -0300

    Correção Rotas

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Cart from './pages/Home';
+import Cart from './pages/Cart';
 
 export default function Routes() {
   return (
