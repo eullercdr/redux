4da962564f2dc077f142dd55289fc73631651114
Euller Cristian <eullercdr@gmail.com>
2020-03-05 09:13:58 -0300

    Criando reducers de cart e configurando os componentes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { formatPrice } from '../../util/format';
@@ -6,9 +7,23 @@ import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
+  };
+
+  /*
+    No momento que o conectamos com o redux ele tem acesso a uma propriedade
+    do this.props que chama dispatch, o dispatch serve para disparar as actions
+    do redux, adicionar algum item, remover, modificar.
+    Passamos um type para a action e a propriedade.
+  */
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
   };
 
   async componentDidMount() {
@@ -32,7 +47,10 @@ export default class Home extends Component {
             <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
             <span>{product.price}</span>
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -44,3 +62,4 @@ export default class Home extends Component {
     );
   }
 }
+export default connect()(Home);
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,17 @@
-export default function cart() {
-  return [];
+/*
+Dentro do reducer, todos os reducers ouve todas as actions.
+
+Fazemos os cases para o reducer ouvir apenas a action que queremos.
+
+No momento que a alteração é feita, todos os componentes com connect são
+avisadas
+*/
+
+export default function cart(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return [...state, action.product];
+    default:
+      return state;
+  }
 }
